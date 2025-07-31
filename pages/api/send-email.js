98bec77b0fce2b84b49f17927f8f2b13d482f8a4
GOKULKRISHNA7868 <gokultupakula@gmail.com>
2025-07-31 14:38:51 +0530

    Initial commit

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
   // CORS Headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3003");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
